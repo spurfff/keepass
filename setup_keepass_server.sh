6d7e4551ce4dd35e7f188345f9b741070f5c44e9
spurfff <spencerprfoster@outlook.com>
2024-02-01 00:48:15 +0000

    Untested still. Beginning here...;;

--- setup_keepass_server.sh
+++ setup_keepass_server.sh
@@ -17,14 +17,55 @@ fi
 
 ssl_dir="/etc/nginx/ssl"
 config_dir="/etc/nginx"
-# Below is the default server block file for nginx
+# Below: the default server block file for nginx
 srv_blk_file="default"
 srv_blk_dir="/etc/nginx/sites-available"
 srv_blk_fullpath="${srv_blk_dir}/${srv_blk_file}"
+# Below: The directory to contain our .kdbx files for later serving
+secure_html="$HOME/secure_html"
 
-# Create a backup of the server block configuratuion file
+# Create a backup of the OG server block configuratuion file
 if [[ ! -f $srv_blk_fullpath.bak ]]; then
 	cp $srv_blk_fullpath $srv_blk_fullpath.bak 
 fi
 
+# Make a super boring ssl self-signed cert
+nginx_key="$ssl_dir/nginx.key"
+nginx_crt="$ssl_dir/nginx.crt"
+if [[ ! -f $niginx_key && ! -f $nginx ]]; then
+	sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $nginx_key -out $nginx_crt
+fi
 
+# Create a new password protected authentication file for the server
+# this can be done using the htpasswd binary from the apache2-utils package
+htpasswd_file="$config_dir/htpasswd"
+sudo htpasswd -c $htpasswd_file $USER
+
+# Create an empty .kdbx file
+kdbx_file="$secure_html/password_database.kdbx"
+touch $kdbx_file
+sudo chown www-data:www-data $secure_html && sudo chown :www-data $secure_html/* && sudo chmod 2770 $secure_html
+
+cat <<EOF > "$srv_blk_fullpath"
+server {
+
+	listen 443 ssl default_server;
+	server_name wired.net;
+	
+	access_log /var/log/nginx/access.log;
+	root $secure_html;
+
+	ssl_certificate $nginx_crt
+	ssl_certificate_key $nginx_key
+
+	location / {
+		auth_basic "Restricted";
+		auth_basic_user_file "$htpasswd_file";
+
+		dav_methods PUT DELETE MOVE COPY;
+		dav_access group:rw all:r;
+	}
+}
+EOF
+
+sudo systemctl restart nginx.service
